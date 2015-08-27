34d5331f8309480009c4dade7f7fdda2fdcbbee9
pirhoo <hello@pirhoo.com>
2015-08-27 13:19:19 +0200

    Freezed style

--- app.js
+++ app.js
@@ -11,27 +11,27 @@ var express = require('express')
 var app = express();
 
 app.configure(function(){
-  
+
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-  
+
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
 
-  app.use(require('less-middleware')({ src: __dirname + '/public' }));
-  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(require('less-middleware')(__dirname + '/public'));
+  app.use(express.static( __dirname + '/public'));
 
   app.use(function(req, res, next) {
     res.locals.path = req.path;
     next();
   });
-  
+
 
   /************************************
-   * Configure router      
-   ************************************/   
+   * Configure router
+   ************************************/
   // @warning Needs to be after helpers
   app.use(app.router);
 });
